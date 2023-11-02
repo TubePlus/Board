caaedb1b102328200039803f5a97a33793ddbb78
김성용 <tjddyd1565@naver.com>
2023-11-02 15:51:23 +0900

    Refactor Exceptionable 반환형태 가독성 향상

--- board/src/main/java/com/tubeplus/board_service/adapter/rdb/persistence/board/BoardPersistence.java
+++ board/src/main/java/com/tubeplus/board_service/adapter/rdb/persistence/board/BoardPersistence.java
@@ -26,100 +26,89 @@ public class BoardPersistence implements BoardPersistent {
 
     @Override
     public Exceptionable<Board, SaveDto> saveBoard(SaveDto dto) {
-        return new Exceptionable<>(this.save, dto);
+        return new Exceptionable<>(this::saveBoardByDto, dto);
     }
+
+    protected Board saveBoardByDto(SaveDto dto) {
+        log.info(dto.toString());
+
+        BoardEntity boardEntity
+                = BoardEntity.builtOf(dto);
+        log.info(boardEntity.toString());
+
+        BoardEntity savedEntity
+                = jpaDataRepo.save(boardEntity);
+        log.info(savedEntity.toString());
+
+        return savedEntity.buildBoard();
+    }
+
 
     @Override
     public Exceptionable<Board, Long> findBoard(Long boardId) {
-        return new Exceptionable<>(this.findBoard, boardId);
+        return new Exceptionable<>(this::findBoardById, boardId);
     }
+
+    protected Board findBoardById(Long boardId) {
+
+        log.info(boardId.toString());
+
+        BoardEntity foundEntity
+                = jpaDataRepo.findById(boardId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_RESOURCE));
+        log.info(foundEntity.toString());
+
+        return foundEntity.buildBoard();
+    }
+
 
     @Override
     public Exceptionable<List<Board>, ListFindDto> findBoardList(ListFindDto dto) {
-        return new Exceptionable<>(this.findBoardList, dto);
+        return new Exceptionable<>(this::findBoardListByDto, dto);
     }
+
+    public List<Board> findBoardListByDto(ListFindDto dto) {
+        log.info(dto.toString());
+
+        List<BoardEntity> foundBoardEntities
+                = queryDslRepo.findBoards(dto);
+        log.info(foundBoardEntities.toString());
+
+        List<Board> foundBoards
+                = foundBoardEntities.stream().map(
+                BoardEntity::buildBoard
+        ).collect(Collectors.toList());
+        foundBoards.forEach(
+                board -> log.info(board.toString())
+        );
+
+        return foundBoards;
+    }
+
 
     @Override
     public Exceptionable<Boolean, Long> completelyDeleteBoard(Long boardId) {
-        return new Exceptionable<>(this.deleteBoard, boardId);
+        return new Exceptionable<>(this::deleteBoardById, boardId);
     }
+
+    public Boolean deleteBoardById(Long boardId) {
+
+        jpaDataRepo.deleteById(boardId);
+
+        return true;
+    }
+
+
+    @Override
+    public Exceptionable<Boolean, Long> softDeleteBoard(Long boardId) {
+        return new Exceptionable<>(queryDslRepo::softDeleteBoard, boardId);
+    }
+
 
     @Override
     public Exceptionable<Boolean, UpdateDto> updateBoard(UpdateDto dto) {
-        return new Exceptionable<>(this.updateBoard, dto);
+        return new Exceptionable<>(queryDslRepo::updateBoard, dto);
     }
 
-    @Override
-    public Exceptionable<Boolean, Long> softlyDeleteBoard(Long boardId) {
-        return new Exceptionable<>(this.logic::softlyDeleteBoard, boardId);
-    }
 
-    private final InternalLogic logic = new InternalLogic();
-
-    public final Function<SaveDto, Board> save = this.logic::saveBoard;
-    public final Function<Long, Board> findBoard = this.logic::findBoard;
-    public final Function<ListFindDto, List<Board>> findBoardList = this.logic::findBoardList;
-    public final Function<UpdateDto, Boolean> updateBoard = this.logic::updateBoard;
-    private final Function<Long, Boolean> deleteBoard = this.logic::deleteBoard;
-
-
-    //todo 접근제어자 private class InternalLogic.public Board functions로 바꿔서 시도
-    public class InternalLogic {
-
-        public Board saveBoard(SaveDto dto) {
-            log.info(dto.toString());
-
-            BoardEntity boardEntity = BoardEntity.builtOf(dto);
-            log.info(boardEntity.toString());
-
-            BoardEntity savedEntity = jpaDataRepo.save(boardEntity);
-            log.info(savedEntity.toString());
-
-            return savedEntity.buildBoard();
-        }
-
-        public Board findBoard(Long boardId) {
-
-            log.info(boardId.toString());
-
-            BoardEntity foundEntity = jpaDataRepo.findById(boardId)
-                    .orElseThrow(() -> new BusinessException(ErrorCode.NOT_FOUND_RESOURCE));
-            log.info(foundEntity.toString());
-
-            return foundEntity.buildBoard();
-        }
-
-        public List<Board> findBoardList(ListFindDto dto) {
-            log.info(dto.toString());
-
-            List<BoardEntity> foundBoardEntities = queryDslRepo.findBoards(dto);
-            log.info(foundBoardEntities.toString());
-
-            List<Board> foundBoards
-                    = foundBoardEntities.stream().map(
-                    BoardEntity::buildBoard
-            ).collect(Collectors.toList());
-            foundBoards.forEach(
-                    board -> log.info(board.toString())
-            );
-
-            return foundBoards;
-        }
-
-        public Boolean updateBoard(UpdateDto dto) {
-            return queryDslRepo.updateBoard(dto);
-        }
-
-        public Boolean deleteBoard(Long boardId) {
-
-            jpaDataRepo.deleteById(boardId);
-
-            return true;
-        }
-
-        public Boolean softlyDeleteBoard(Long boardId) {
-
-            return queryDslRepo.softDeleteBoard(boardId);
-        }
-    }
 }
