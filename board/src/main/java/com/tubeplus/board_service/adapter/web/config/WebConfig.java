66917afe56b517bfd2216ea31c46e26705da58ee
Seong yong , Kim <69845367+Soeng-dev@users.noreply.github.com>
2023-11-13 11:19:15 +0900

    Fix: cors

--- board/src/main/java/com/tubeplus/board_service/adapter/web/config/WebConfig.java
+++ board/src/main/java/com/tubeplus/board_service/adapter/web/config/WebConfig.java
@@ -10,8 +10,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // CORS를 적용할 URL 패턴 정의
+                .allowedOrigins("*") // 허용할 출처
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // 허용할 HTTP method
+                .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
+    }
 
-
+    
     protected void addBoardFormatters(FormatterRegistry registry) {
 
         registry.addConverter(new BoardSearchType.ReqConverter());
