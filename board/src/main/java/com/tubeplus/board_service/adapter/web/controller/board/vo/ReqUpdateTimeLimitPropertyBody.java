92643b9b7228afc2d60e8469f88b43bbadd7dea6
김성용 <tjddyd1565@naver.com>
2023-11-16 13:38:46 +0900

    Feat: update Time limit property 유효성 검증 로직 추가

--- board/src/main/java/com/tubeplus/board_service/adapter/web/controller/board/vo/ReqUpdateTimeLimitPropertyBody.java
+++ board/src/main/java/com/tubeplus/board_service/adapter/web/controller/board/vo/ReqUpdateTimeLimitPropertyBody.java
@@ -4,12 +4,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.tubeplus.board_service.application.board.port.in.BoardUseCase;
 import com.tubeplus.board_service.application.board.port.in.BoardUseCase.BoardProperty.TimeLimitBoardProperty;
 import lombok.Data;
+import lombok.NonNull;
 import lombok.Value;
 
 import java.time.LocalDateTime;
 
 @Data
 public class ReqUpdateTimeLimitPropertyBody {
+
+    @NonNull
     private LocalDateTime limitDateTime;
 
     public TimeLimitBoardProperty toDomain() {
