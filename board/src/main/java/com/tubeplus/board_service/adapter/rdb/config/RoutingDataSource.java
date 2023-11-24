8bfe21d5cff58a29dff5f914f42652741b4daf5f
김성용 <tjddyd1565@naver.com>
2023-11-24 21:55:23 +0900

    Refactor: master-slave db replica 설정

---
+++ board/src/main/java/com/tubeplus/board_service/adapter/rdb/config/RoutingDataSource.java
@@ -0,0 +1,23 @@
+package com.tubeplus.board_service.adapter.rdb.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@Slf4j
+public class RoutingDataSource extends AbstractRoutingDataSource {
+    @Override
+    protected Object determineCurrentLookupKey() {
+
+        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+
+        if (isReadOnly) {
+            log.info("slave db 동작 ==>");
+        } else {
+            log.info("master db 동작 ==>");
+        }
+
+        return isReadOnly ? "slave" : "master";
+    }
+}
